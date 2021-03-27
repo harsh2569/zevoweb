834d5bb67c3c04abad1d52bac228ecd01f175b57
Arun <arunram20001136@gmail.com>
2021-03-27 12:55:02 +0530

    'notification'

--- src/patterns/contact.js
+++ src/patterns/contact.js
@@ -33,7 +33,6 @@ const Contact = () => {
     }
 
     function validate(){
-        console.log(formData.username,formData.email,formData.mobileno)
         if(formData.username==="" || formData.mobileno==="" || formData.email===""){
             toast.error("please fill the empty fields");
             return false; 
@@ -59,7 +58,12 @@ const Contact = () => {
                 username,work,place,category,duration,amount,email,mobileno
             }
 
-            emailjs.send('zevocorp_service', 'template_2bco5dd', templateParams , 'user_pA3AcBwuZrONMUHbSNCIo')
+            emailjs.send(
+                'zevocorp_service', 
+                'template_2bco5dd', 
+                templateParams , 
+                'user_pA3AcBwuZrONMUHbSNCIo'
+            )
             .then((result) => {
                 toast.dark(
                     <div>
@@ -86,42 +90,131 @@ const Contact = () => {
         <div className="contact">
 
             <p className="block-title">Get in touch with us</p>
-            <p style={{margin:'1em 0'}}>Send us a message, get a quote from us.</p>
-            <li><img src={hello} alt="hello" /><span>Hello folks at ZEVO Corporation!</span></li>
+            <p style={{margin:'1em 0'}}>
+                Send us a message, get a quote from us.
+            </p>
+            <li>
+                <img src={hello} alt="hello" />
+                <span>Hello folks at ZEVO Corporation!</span>
+            </li>
 
             <form onSubmit={handleSubmit}>
             <div className="input-group">
                 My &nbsp; name &nbsp; is 
-                <input type="text" placeholder="Eg. Esther Howard" name="username" value={formData.username} onChange={handleChange} />. 
+
+                <input 
+                    type="text" 
+                    placeholder="Eg. Esther Howard" 
+                    name="username" 
+                    value={formData.username} 
+                    onChange={handleChange} 
+                />. 
+
                 I &nbsp; Work &nbsp; as &nbsp; a
-                 <input type="text" placeholder="Eg. Cheif Technical Officer" name="work"  value={formData.work} onChange={handleChange}  />
+
+                 <input 
+                    type="text" 
+                    placeholder="Eg. Cheif Technical Officer" 
+                    name="work"  
+                    value={formData.work} 
+                    onChange={handleChange}  
+                />
+
                  &nbsp; at 
-                 <input type="text" placeholder="Eg. Antus Data Labs, USA" name="place"  value={formData.place} onChange={handleChange} />
+
+                 <input 
+                    type="text" 
+                    placeholder="Eg. Antus Data Labs, USA" 
+                    name="place"  
+                    value={formData.place} 
+                    onChange={handleChange} 
+                />
+
                We &nbsp; have &nbsp; a &nbsp; budget &nbsp; of &nbsp; around 
-                <input type="number" placeholder="Enter your estimated budget" name="amount"  value={formData.amount} onChange={handleChange} />
+
+                <input 
+                    type="number" 
+                    placeholder="Enter your estimated budget" 
+                    name="amount"  
+                    value={formData.amount} 
+                    onChange={handleChange} 
+                />
+
                 to &nbsp; complete &nbsp; a
-                 <select name="category"  value={formData.category} onChange={handleChange}>
-                     <option value="" disabled  hidden>Select a project category</option>
-                     <option value="UI | UX Designing">UI | UX Designing</option>
-                     <option value="Hybrid App Development">Hybrid App Development</option> 
-                     <option value="Progressive Web App Development">Progressive Web App Development</option>
-                     <option value="Landing Page Development">Landing Page Development</option>
-                     <option value="Minimum Viable Product Development">Minimum Viable Product Development</option>
-                     <option value="Blockchain Core Development">Blockchain Core Development</option>
-                     <option value="Smart Contract Development">Smart Contract Development</option>
-                     <option value="Defi / Dapp / NFT Development">Defi / Dapp / NFT Development</option>
-                     <option value="Web3 / Ethers Integration">Web3 / Ethers Integration</option>
-                     <option value="Dapp Architechting">Dapp Architechting</option>
+
+                 <select 
+                    name="category"  
+                    value={formData.category} 
+                    onChange={handleChange}
+                 >
+                     <option value="" disabled  hidden>
+                         Select a project category
+                    </option>
+                     <option value="UI | UX Designing">
+                         UI | UX Designing
+                    </option>
+                     <option value="Hybrid App Development">
+                         Hybrid App Development
+                    </option> 
+                     <option value="Progressive Web App Development">
+                         Progressive Web App Development
+                    </option>
+                     <option value="Landing Page Development">
+                         Landing Page Development
+                    </option>
+                     <option value="Minimum Viable Product Development">
+                         Minimum Viable Product Development
+                    </option>
+                     <option value="Blockchain Core Development">
+                         Blockchain Core Development
+                    </option>
+                     <option value="Smart Contract Development">
+                         Smart Contract Development
+                    </option>
+                     <option value="Defi / Dapp / NFT Development">
+                         Defi / Dapp / NFT Development
+                    </option>
+                     <option value="Web3 / Ethers Integration">
+                         Web3 / Ethers Integration
+                    </option>
+                     <option value="Dapp Architechting">
+                         Dapp Architechting
+                    </option>
                 </select> 
+
                  on &nbsp; or &nbsp; around &nbsp; a
-                <input type="text" placeholder="Eg. 2 months from now" name="duration"  value={formData.duration} onChange={handleChange} />
+
+                <input 
+                    type="text" 
+                    placeholder="Eg. 2 months from now" 
+                    name="duration"  
+                    value={formData.duration} 
+                    onChange={handleChange} 
+                />
+
                 .&nbsp;You &nbsp; can &nbsp; reach &nbsp; me &nbsp; at
-                <input type="text" placeholder="Eg. estherhoward@example.com" name="email"  value={formData.email} onChange={handleChange} /> 
+
+                <input 
+                    type="text" 
+                    placeholder="Eg. estherhoward@example.com" 
+                    name="email"  
+                    value={formData.email} 
+                    onChange={handleChange} 
+                /> 
+
                 or &nbsp; at 
+                
                 <select>
                     <option value="" disabled selected hidden>91</option>
                 </select>
-                <input type="number" placeholder="Eg. 7789654123" name="mobileno"  value={formData.mobileno} onChange={handleChange} />
+
+                <input 
+                    type="number" 
+                    placeholder="Eg. 7789654123" 
+                    name="mobileno"  
+                    value={formData.mobileno} 
+                    onChange={handleChange} 
+                />
             </div>
             <div className="block-end">
                 <div>
